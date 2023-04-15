3d0ac017a43dbb57b6981244912e5fe7e0a180ae
Nikolas Kovacs <kovacsn00@gmail.com>
2023-04-15 17:17:11 -0400

    debug logs

--- public/sockets.js
+++ public/sockets.js
@@ -3,11 +3,12 @@ let socket = io();
 // store other players
 let players = {};
 
-socket.on('connect', function (socket) {
+socket.on('connect', function () {
     console.log('Connected to server');
 });
 
 socket.on("spawnPlayer", (id, x, y) => {
+    console.log(`Spawning player ${id} at (${x}, ${y})`)
     players[id] = {
         x: x,
         y: y
@@ -15,19 +16,23 @@ socket.on("spawnPlayer", (id, x, y) => {
 });
 
 socket.on("despawnPlayer", (id) => {
+    console.log(`Despawning player ${id}`)
     delete players[id];
 });
 
 socket.on("updatePlayerPosition", (id, x, y) => {
+    console.log(`Updating player ${id} position to (${x}, ${y})`)
     players[id].x = x;
     players[id].y = y;
 });
 
 socket.on("updatePlayerRotation", (id, y_rot) => {
+    console.log(`Updating player ${id} rotation to ${y_rot}`)
     players[id].y_rot = y_rot;
 });
 
 socket.on("updatePlayerAnimation", (id, animation) => {
+    console.log(`Updating player ${id} animation to ${animation}`)
     players[id].animation = animation;
 });
 
