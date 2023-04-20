50d15db031c7442411d9d787bd9eb8f379f5f771
Nikolas Kovacs <kovacsn00@gmail.com>
2023-04-19 22:49:10 -0400

    load model function

--- public/tools.js
+++ public/tools.js
@@ -160,44 +160,20 @@ function calc_normals(coords, indices, is_tri_strip) {
 }
 
 /**
- * Convert OBJ data (as a single string) to a dictionary containing the
- * "vertices" and "indices" as two lists. This only uses the vertex and face
- * data, ignoring all normals, texture coordinates, materials and everything
- * else. Vertices must be 3D and faces must be triangles.
+ * Load a model from a file into a VAO and return the VAO.
  */
-function obj2dict(obj_text) {
-    let vertices = [];
-    let indices = [];
-    let normals = [];
-    for (let line of obj_text.split('\n')) {
-        // remove comments and break up on whitespace
-        let data = line.split('#', 1)[0].trim().split(/\s+/);
-        // skip empty lines
-        if (data.length === 0) { continue }
-        if (data[0] === 'v') {
-            // vertex line
-            if (data.length !== 4)
-                throw `All vertices must be 3D, vertex ${vertices.length/3} is ${data.length-1}D`;
-            vertices.push(...data.slice(1).map(x => +x));
-        } else if (data[0] === 'f') {
-            // face line
-            if (data.length !== 4)
-                throw `All faces must be triangles, face ${indices.length/3} has ${data.length-1} vertices`;
-            indices.push(...data.slice(1).map(x => parseInt(x.split('/', 1)[0])-1));
-        } else if (data[0] === 'vn') {
-            // normals
-            if (data.length !== 4)
-                throw `All normals must be 3D, normal ${normals.length/3} is ${data.length-1}D`;
-            normals.push(...data.slice(1).map(x => +x));
-        } else if (data[0] === 'vt') {
-            // texture coordinates
-        } else if (data[0] === "usemtl") {
-            // mtl - corresponding material and texture
-        } else {
-            // all other lines are ignored
-            console.debug('ignoring', line);
-        }
-    }
-    // return the results
-    return {"vertices": vertices, "indices": indices, "normals": normals};
+function loadModel(filename) {
+    return fetch(filename)
+        .then(r => r.json())
+        .then(raw_model => {
+            let coords = new Float32Array(raw_model.vertices);
+            let inds = new Uint16Array(raw_model.indices);
+            let texCoords = new Float32Array(raw_model.texels);
+            let normals = calc_normals(coords, inds, false);
+
+            return [coords, inds, normals, texCoords, raw_model.texture]
+
+        })
+        // eslint-disable-next-line no-console
+        .catch(console.error);
 }
